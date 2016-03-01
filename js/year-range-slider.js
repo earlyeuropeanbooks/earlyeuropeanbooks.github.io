e6c1335f0ac83e04d8fd7b75069766fa165140e1
duhaime <douglas.duhaime@gmail.com>
2016-03-01 12:06:29 -0500

    add callback function for year range slider

--- js/year-range-slider.js
+++ js/year-range-slider.js
@@ -1,10 +1,12 @@
-// With JQuery
-$("#year-range-slider").ionRangeSlider({
+// the onFinish method is called when a user
+// finishes interacting with the range slider 
+var yearRangeSlider = $("#year-range-slider").ionRangeSlider({
     type: "double",
     min: 1473,
     max: 1700,
     step: 1,
-    prettify_separator: ""
+    prettify_separator: "",
+    onFinish: function(r) {
+      console.log(r);
+    }
 });
-
-
