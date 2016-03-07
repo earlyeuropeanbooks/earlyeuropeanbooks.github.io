cf941c4a2399bbbac737a05ece32281f22236d3e
Douglas Duhaime <douglas.duhaime@gmail.com>
2016-03-06 19:13:24 -0500

    add js for opacity slider, and update map max and min zoom controls

--- js/eebMap.js
+++ js/eebMap.js
@@ -6,7 +6,9 @@ var initializeMap = function() {
  // add basemap to the #map id div
   var map = new L.Map("map", {
       center: new L.LatLng(46.85, 7.35),
-      zoom: 5
+      zoom: 5,
+      maxZoom: 10,
+      minZoom: 3
   });
 
   /***********
@@ -15,7 +17,6 @@ var initializeMap = function() {
 
   // add map layer for black and white background
   map.addLayer(new L.tileLayer("http://korona.geog.uni-heidelberg.de/tiles/roadsg/x={x}&y={y}&z={z}", {
-    maxZoom: 8,
     attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">' +
         'GIScience Research Group @ University of Heidelberg</a> &mdash; ' +
         'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -25,7 +26,6 @@ var initializeMap = function() {
   * Shapefile overlay *
   ********************/
 
-  /*
   // add a map layer with the colored background tiles
   d3.json("/json/geojson/europeanCountries.geojson", function(error, json) {
     if (error) return console.warn(error);
@@ -57,23 +57,29 @@ var initializeMap = function() {
     // add the geoJson with data to the map
     L.geoJson(json, {style: style}).addTo(map);    
   });
-  */
 
   /****************
   * Image Overlay *
   ****************/
 
   // specify the path to the tms image tiles to be overlaid on the map
-  var imageTileUrl = "https://s3.amazonaws.com/eeb-map/carl-radefeld-1843/{z}/{x}/{y}.png"
+  var imageTileUrl = "https://s3.amazonaws.com/eeb-map/carl-radefeld-1843/{z}/{x}/{y}.png";
+
+  var imageTileUrl = "/images/carlRadefeld1843Tiles/{z}/{x}/{y}.png";
   
   // add the image tiles to the map
-  L.tileLayer(imageTileUrl, {
-    minZoom: 5,
-    maxZoom: 8,
-    attribution: "Open Source",
-    opacity: .6,
+  var imageTileLayer = L.tileLayer(imageTileUrl, {
+    attribution: "",
     tms: true
   }).addTo(map);
+
+  // add a class to the image tile layer for dynamic css styling
+  $(imageTileLayer.getContainer()).addClass('imageTileLayer');
+
+  // add an opacity slider to the imageTileLayer
+  var opacitySlider = new L.Control.opacitySlider();
+    map.addControl(opacitySlider);
+    opacitySlider.setOpacityLayer(imageTileLayer);
 
   // add the points to the populated map
   d3.json("/json/page_load_book_locations.json", function(error, bookLocationJson) {
