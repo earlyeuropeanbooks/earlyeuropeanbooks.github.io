2090ac1a39c8e7e938a56703b4f985c4117517f2
duhaime <douglas.duhaime@gmail.com>
2016-03-28 20:49:47 -0400

    update year range slider to wrap addMapPoints call in setTimeout wrapper

--- js/rangeSlider/yearRangeSlider.js
+++ js/rangeSlider/yearRangeSlider.js
@@ -1,21 +1,31 @@
 // the onFinish method is called when a user
-// finishes interacting with the range slider 
+// finishes interacting with the range slider, 
+// but if we're already drawing the map, then wait to
+// draw the new layer
+var updateYearRange = function(result) {
+  if (globalDrawingMap === 1) {
+    setTimeout(function(){updateYearRange(result)}, 100);
+  
+  } else {
+    var startYear = result.from;
+    var endYear = result.to;
+    addMapPoints(globalLastJson, startYear, endYear);  
+  }
+};
+
 $("#year-range-slider").ionRangeSlider({
     type: "double",
     min: 1473,
     max: 1700,
     step: 1,
     prettify_separator: "",
-    
+
     onFinish: function(result) {
       // once user finishes interacting with the range
       // slider, redraw the markers, retaining only
       // those that fall within the user-specified year range
-      var startYear = result.from;
-      var endYear = result.to;
-
-      addMapPoints(globalLastJson, startYear, endYear);
-  } 
+      updateYearRange(result);
+    } 
 });
 
 // create variable so that we can call yearRangeSlider.reset()
